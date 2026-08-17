fb86933330892e60056598c148900d7be4e7d52f
mohammed <mohammedoukir14@gmail.com>
2026-08-17 19:11:51 +0100

    erreur crction

--- backend/Mavex-Medafrica/mavex/src/main/java/com/medafrica/mavex/controller/OrderStatusHistoryController.java
+++ backend/Mavex-Medafrica/mavex/src/main/java/com/medafrica/mavex/controller/OrderStatusHistoryController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/order-history")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class OrderStatusHistoryController {
 
     private final OrderStatusHistoryService historyService;
@@ -19,7 +20,6 @@ public class OrderStatusHistoryController {
     // ─────────── GET /api/order-history/order/{orderId} ───────────
     /** Retourne tout l'historique des statuts d'un order donné */
     @GetMapping("/order/{orderId}")
-    //@PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
     public ResponseEntity<List<OrderStatusHistoryResponse>> getByOrder(@PathVariable Long orderId) {
         return ResponseEntity.ok(historyService.getHistoryByOrder(orderId));
     }
@@ -27,7 +27,6 @@ public class OrderStatusHistoryController {
     // ─────────── GET /api/order-history/user/{userId} ───────────
     /** Retourne toutes les actions d'un utilisateur (audit ADMIN) */
     @GetMapping("/user/{userId}")
-    //@PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<OrderStatusHistoryResponse>> getByUser(@PathVariable Long userId) {
         return ResponseEntity.ok(historyService.getHistoryByUser(userId));
     }
--- backend/Mavex-Medafrica/mavex/src/main/java/com/medafrica/mavex/controller/UserController.java
+++ backend/Mavex-Medafrica/mavex/src/main/java/com/medafrica/mavex/controller/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +17,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     private final UserService userService;
 
     /** GET /api/users */
-    /**  @PreAuthorize("hasRole('ADMIN')")*/
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> getAll() {
         return ResponseEntity.ok(userService.findAll());
